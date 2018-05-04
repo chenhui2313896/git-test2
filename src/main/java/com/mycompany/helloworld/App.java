9881b69fa6a11feefa991748074d06bce8d65406
394498036@qq.com <394498036@qq.com>
2018-05-04 21:47:55 +0800

    自动构建

--- src/main/java/com/mycompany/helloworld/App.java
+++ src/main/java/com/mycompany/helloworld/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-	System.out.println( "hello jenkins!" );
+	System.out.println( "success Hello jenkins!" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello to myself!" );
         System.out.println( "Finishing CI Course" );
