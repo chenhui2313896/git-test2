21c3bad58cf0358964f853974e15b2e9cba17e09
root <root@dsiz-phpidig8.novalocal>
2018-05-04 21:24:42 +0800

    tijiao

--- src/main/java/com/mycompany/helloworld/App.java
+++ src/main/java/com/mycompany/helloworld/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-	System.out.println( "»¶Ó­¹Ø×¢£º±à³Ì¿ÓÌ«¶à!" );
+	System.out.println( "hello jenkins!" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello to myself!" );
         System.out.println( "Finishing CI Course" );
